68466f6d3a5a89d896721d247d0ee08f522d5927
Rafaela Augusto S <rafaela.augusto@somei.com.vc>
2022-04-06 16:51:11 -0300

    migration of book

--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,10 +1,6 @@
 package com.mercadolivro.model
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity(name = "customer")
 class CustomerModel(
